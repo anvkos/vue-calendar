ead5acef8449165df06e0e5028b99d767e378809
Anton Kosolapov <anvkos@ya.ru>
2017-08-12 13:16:11 +0700

    +: 22 generate list days current month

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,13 +1,48 @@
 <template>
-    <div>{{ msg }}</div>
+    <div>
+        <div v-for="day in days">{{ day }}</div>
+    </div>
 </template>
 
 <script>
     export default {
         data() {
             return {
-                msg: 'Hello World'
+                month: 8,
+                year: 2017
             };
+        },
+        computed: {
+            days() {
+                // Generating days in current month
+                let days = [];
+                let currentDay = this.$moment(`${this.year}-${this.month}-1`, 'YYYY-M-D');
+                do {
+                    days.push(currentDay);
+                    currentDay = this.$moment(currentDay).add(1, 'days');
+                } while ((currentDay.month() + 1) === this.month);
+
+                const SUNDAY = 0;
+                const MONDAY = 1;
+                // Add previous days to start
+                currentDay = this.$moment(days[0]);
+                if (currentDay.day() !== MONDAY) {
+                    do {
+                        currentDay = this.$moment(currentDay).subtract(1, 'days');
+                        days.unshift(currentDay);
+                    } while (currentDay.day() !== MONDAY);
+                }
+
+                // Add following days to end
+                currentDay = this.$moment(days[days.length - 1]);
+                if (currentDay.day() !== SUNDAY) {
+                    do {
+                       currentDay = this.$moment(currentDay).add(1, 'days');
+                       days.push(currentDay);
+                    } while (currentDay.day() !== SUNDAY);
+                }
+                return days;
+            }
         }
     }
 </script>
