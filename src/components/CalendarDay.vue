d5f3d28c498530fac28a7e7c52175e397b934e2b
Anton Kosolapov <anvkos@ya.ru>
2017-08-12 16:22:36 +0700

    +: 23 UI effect for days

--- src/components/CalendarDay.vue
+++ src/components/CalendarDay.vue
@@ -1,12 +1,21 @@
 <template>
-    <div class="day">
+    <div :class="classObject">
         {{ day.format('D') }}
     </div>
 </template>
 
 <script>
     export default {
-        props: [ 'day' ]
-
+        props: [ 'day' ],
+        computed: {
+            classObject() {
+                let today = this.day.isSame(this.$moment(), 'day')
+                return {
+                    day: true,
+                    today,
+                    past: this.day.isSameOrBefore(this.$moment(), 'day') && !today
+                }
+            }
+        }
     }
 </script>
