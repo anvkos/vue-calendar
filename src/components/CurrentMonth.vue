61b95d7679506baef3c9500056d464739dc047cb
Anton Kosolapov <anvkos@ya.ru>
2017-08-14 21:45:33 +0700

    +: 24 Changing month and year #vuex

--- src/components/CurrentMonth.vue
+++ src/components/CurrentMonth.vue
@@ -9,8 +9,22 @@
 <script>
     export default {
         methods: {
-            decriment() {},
-            increment() {}
+            decriment() {
+                if (this.month === 1) {
+                    this.$store.commit('setCurrentMonth', 12);
+                    this.$store.commit('setCurrentYear', this.year - 1);
+                } else {
+                    this.$store.commit('setCurrentMonth', this.month - 1);
+                }
+            },
+            increment() {
+                if (this.month === 12) {
+                    this.$store.commit('setCurrentMonth', 1);
+                    this.$store.commit('setCurrentYear', this.year + 1);
+                } else {
+                    this.$store.commit('setCurrentMonth', this.month + 1);
+                }
+            }
         },
         computed: {
             formattedDate() {
