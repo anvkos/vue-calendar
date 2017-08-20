067893c073f09312f47c32ee004c4f812ea99db8
Anton Kosolapov <anvkos@ya.ru>
2017-08-20 11:23:11 +0700

    +: 28 Common entry file

--- src/web.entry.js
+++ src/web.entry.js
@@ -1,40 +1,13 @@
-import Vue from 'vue';
 import './style.scss';
-
-import store from './store';
-
 import moment from 'moment-timezone';
 moment.tz.setDefault('UTC');
-Object.defineProperty(Vue.prototype, '$moment', { get() { return this.$root.moment } })
 
 let events = window._INITIAL_STATE_.map(event => {
   return {
     description: event.description,
     date: moment(event.date)
   }
-})
-
-let initialState = Object.assign({}, store.state, { events });
-store.replaceState(initialState);
-
-import App from './components/App.vue';
-
-new Vue({
-    el: '#app',
-    data: {
-        moment
-    },
-    components: {
-        App
-    },
-    store,
-    render (createElement) {
-        return createElement(
-            'div',
-            { attrs: { id: 'app'} },
-            [
-                createElement('app')
-            ]
-        );
-    }
 });
+
+import VueCalendar from './entry';
+VueCalendar(events).$mount('#app');
